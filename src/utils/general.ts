9dc944beaedac0648df74f3bbfb77536f902ac04
Rogi Solorzano <rogi.solorzano@hingehealth.com>
2022-12-02 00:12:25 -0600

    Day 2

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,3 +1,9 @@
 export const isDefined = <T>(v: T | undefined | null): v is T => typeof v !== 'undefined' && v !== null;
 
 export const wait = async (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+export const invert = <K extends keyof any, V extends keyof any>(map: Record<K, V>): Record<V, K> =>
+  Object.entries(map).reduce((inverted, [k, v]) => {
+    inverted[v as V] = k as K;
+    return inverted;
+  }, {} as Record<V, K>);
